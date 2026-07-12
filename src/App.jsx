c68a70661755bb5f628e2e8c0c3f4db078382e3f
adrian <adrian>
2026-07-12 14:57:28 +0200

    feat: taskForm, dataLifting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import TaskList from './components/taskList.jsx'
 import TaskForm from './components/taskForm.jsx'
 
 function App() {
-  
+  const [dataList, setDataList] = useState([])
+
+  useEffect(() => {
+    console.log(dataList)
+  }, [dataList])
+
+  const addTask = (taskObj) => {
+    setDataList(prev=> [...prev, taskObj])
+  }
+
   return (
-    <div className='main-container'>
+    <>
       <h1 className='title'>Task Manager v01</h1>
-      <TaskForm />
-      <TaskList />
-    </div>
+      <div className='main-container'>
+        
+
+        <div className='form-container'>
+          <TaskForm addtask={addTask}/>
+        </div>
+
+        <div className='list-container'>
+          <TaskList />
+        </div>
+
+      </div>
+    </>
   )
 }
 
--- src/components/taskForm.jsx
+++ src/components/taskForm.jsx
@@ -1,7 +1,31 @@
+import './taskForm.css'
+import {useState} from 'react'
 
+export default function TaskForm (props) {
+    const addTask = props.addtask
+    const [currentVal, setVal] = useState('')
 
-export default function TaskForm () {
+    const handleChange = (e) => {
+        setVal(e.target.value)
+    }
 
-    return 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const dataObj = {
+            title: currentVal,
+            checked: false,
+        }
+        addTask(dataObj)
+        setVal('')
+    }
+
+    return (
+        <div className='component'>
+            <form onSubmit={handleSubmit}>
+              <input type='text' value={currentVal} onChange={handleChange} placeholder='Write your task here...'/>
+              <button type='submit'>Add</button>
+            </form>
+        </div>
+    )
 }
 

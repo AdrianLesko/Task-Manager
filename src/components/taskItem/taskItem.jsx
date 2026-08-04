b086769c6e6ee2847350ef79a776736cb82f1da4
Adrian Lesko <lesko.ado.adrian@gmail.com>
2026-08-04 18:02:39 +0200

    implement tailwind styling on taskItem.jsx

--- src/components/taskItem/taskItem.jsx
+++ src/components/taskItem/taskItem.jsx
@@ -1,22 +1,27 @@
-import './taskItem.css'
 
 function TaskItem ({task, toggleTask, deleteTask, editTask}) {
 
     
     return (
-        <div className='task-container'>
-            <div className='task-container-texts'>
-                <h2>{task.title}</h2>
-                <p>{task.description}</p>
+        <div className='flex flex-row border-2 border-secondary rounded-lg mt-1 justify-between pl-2
+            pr-3 items-center bg-white
+         '>
+            <div className='flex flex-row items-center justify-start gap-2'>
+                <div className=''>
+                    <input className='hover:cursor-pointer' type='checkbox' checked={task.completed} onChange={() => toggleTask(task)}/>
+                </div>
+                <div className='flex flex-col items-start justify-center'>
+                    <h2 className='font-extrabold text-xl text-primary'>{task.title}</h2>
+                    <p>{task.description}</p>
+                </div>
             </div>
-            <div className='task-container-interactivity'>
-                <h3>{task.priority}</h3>
-                <h3>{task.completed ? '✅ completed' : 'not completed'}</h3>
+            <div className='flex flex-row gap-10'>
+                <h3 className='bg-primary-b px-2 py-1 rounded-xl cursor-default text-white text-[13px] font-bold'>{task.priority}</h3>
+                <h3 className='bg-primary-b px-2 py-1 rounded-xl cursor-default text-white text-[13px] font-bold'>{task.completed ? '✅ completed' : 'not completed'}</h3>
                 
-                <div className='btns-task-container'>
-                    <input type='checkbox' checked={task.completed} onChange={() => toggleTask(task)}/>
-                    <button onClick={() => editTask(task)}>Edit</button>
-                    <button onClick={() => deleteTask(task)}>Delete</button>
+                <div className='flex flex-row gap-2'>
+                    <button className='bg-primary-b px-4 py-1 rounded-xl text-white text-[13px] cursor-pointer font-bold hover:bg-secondary hover:text-black' onClick={() => editTask(task)}>Edit</button>
+                    <button className='bg-primary-b px-4 py-1 rounded-xl text-white text-[13px] cursor-pointer font-bold hover:bg-red-400 hover:text-black'onClick={() => deleteTask(task)}>Delete</button>
                 </div>
             </div>
         </div>
